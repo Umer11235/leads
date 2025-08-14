edf2fee680607dd04a690327a50ec1c702069c1c
Umer Nawaz <abhay.nawaz@gmail.com>
2025-08-14 18:38:38 +0500

    update

--- app/(pages)/lead/page.tsx
+++ app/(pages)/lead/page.tsx
@@ -161,7 +161,7 @@ const [initialData, setInitialData] = useState<IContactFormValues>({
     console.log("updated payloadss" ,updatePayload )
     // First update the current record
      setselectedPrevId(id);
-     alert(id)
+ 
     const updateResponse = await apiService.putData(
       `/Contact/${id}`, 
       updatePayload, 
